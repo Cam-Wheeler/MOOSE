c75fef8e5daa7af56bd8383a8502ea22e60f3738
Cam-Wheeler <cam010100@hotmail.co.uk>
2025-11-18 21:53:32 +0000

    Addition of cache to testing CI, removal of failing test to test workflow

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,11 +3,11 @@
 def pass_test():
     return True
 
-def fail_test():
-    return False
+# def fail_test():
+    # return False
 
 def test_passing_function() -> None:
     assert pass_test() == True
 
-def test_failing_function() -> None:
-    assert fail_test() == True
+# def test_failing_function() -> None:
+    # assert fail_test() == True
