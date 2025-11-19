551dce23189722f4ae2675dbef9236430dee1489
Cam-Wheeler <cam010100@hotmail.co.uk>
2025-11-19 10:48:43 +0000

    Addition of data dir in tests and very basic e2e test

---
+++ tests/test_e2e.py
@@ -0,0 +1,13 @@
+"""Testing End 2 End."""
+import pytest
+from pathlib import Path
+from moosez import moose
+
+data = Path(__file__).parent / "data"
+
+def test_basic_e2e(tmp_path) -> None:
+    min_ct_throat = data / "CT_enhance_pet_1446_neck_slice.nii.gz"
+    moose(input_data=str(min_ct_throat),
+          model_names="clin_ct_organs",
+          output_dir=tmp_path,
+          accelerator="cpu")
